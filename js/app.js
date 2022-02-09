4b9cfa1bcb430a4c33aa828a67e5fbd6ba7c2b68
Jon Gitter <jonathan.m.gitter@gmail.com>
2022-02-09 00:16:07 -0700

    initial questions, included lower case, and let/if/else if structure

--- js/app.js
+++ js/app.js
@@ -1,2 +1,42 @@
 'use strict'
 
+let responseOne = prompt('Hi! I\'m glad you stumbled across my website. Let\'s get to know each other. What\'s your name?');
+alert('Welcome ' + responseOne + '!');
+
+alert('I have a few questions for ya before you get to know me.');
+
+let responseTwo = prompt('Are you here to give me a job?').toLowerCase();
+if (responseTwo === 'yes' || responseTwo === 'y') {
+  alert('Great! I could use a nice fat paycheck!');
+} else if (responseTwo === 'no' || responseTwo === 'n') {
+  alert('Thats a bummer friend, I need a job.');
+}
+
+let responseThree = prompt('Is your favorite college Texas A&M?').toLowerCase();
+if (responseThree === 'yes' || responseThree === 'y') {
+  alert('Awesome! Always great to meet another Aggie!');
+} else if (responseThree === 'no' || responseThree === 'n') {
+  alert('Looks like you\'re not coming to the football party...');
+}
+
+let responseFour = prompt('Is you favorite food steak?').toLowerCase();
+if (responseFour === 'yes' || responseFour === 'y') {
+  alert('Great taste! My favorite food is steak too!');
+} else if (responseFour === 'no' || responseFour === 'n') {
+  alert('It\'s a real shame we couldn\'t agree on chow.');
+}
+
+let responseFive = prompt('Are you here to give me a job?').toLowerCase();
+if (responseFive === 'yes' || responseFive === 'y') {
+  alert('Great! I could use a nice fat paycheck!');
+} else if (responseFive === 'no' || responseFive === 'n') {
+  alert('Thats a bummer friend, I need a job.');
+}
+
+let responseSix = prompt('Are you here to give me a job?').toLowerCase();
+if (responseSix === 'yes' || responseSix === 'y') {
+  alert('Great! I could use a nice fat paycheck!');
+} else if (responseSix === 'no' || responseSix === 'n') {
+  alert('Thats a bummer friend, I need a job.');
+}
+
